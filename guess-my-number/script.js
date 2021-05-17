a5d465e4d271727fd98c27e9206f884d5f438af4
Hunter Marsh <marshsh@gmail.com>
2021-05-17 17:24:48 -0400

    Added if the guess was too high or low, checks if the score reaches 0, and added a score variable so the score is tracked in the code instead of relying on the DOM to hold that info

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,25 +1,39 @@
 'use strict';
 
 // Initalizes the first random number on page load.  New random numbers are generated via 'Again!' button.
-let numberCorrect = Math.floor(Math.random() * 10) + 1;
+let numberCorrect = Math.trunc(Math.random() * 10) + 1;
 console.log(numberCorrect);
+let score = 20;
 
 // Function that is the main game logic.  Checks if a guess was entered.  Compares the .guess.value to the RNG number.  Wrong guesses decrement score.  Correct guesses change message text and compares to highscore.
+// Added a score variable above to keep track of the data with code instead of relying on the DOM to keep track of it.  Also added if a guess was too high or too low, and what happens when score reaches 0.  It checks if the score has reached 0 earlier so that it is no longer possible to win after losing.
 const numberGuess = function () {
-  const score = document.querySelector('.score');
   const highscore = document.querySelector('.highscore');
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
     document.querySelector('.message').textContent = 'No number given...';
+  } else if (score <= 1) {
+    document.querySelector('.message').textContent = 'You lost the game';
+    document.querySelector('.score').textContent = 0;
   } else if (guess === numberCorrect) {
     document.querySelector('.message').textContent = 'Yes!';
     document.querySelector('.number').textContent = numberCorrect;
-    if (highscore.textContent < score.textContent) {
-      highscore.textContent = score.textContent;
+    if (highscore.textContent < score) {
+      highscore.textContent = score;
     }
+    score = 20;
   } else {
-    document.querySelector('.message').textContent = 'Wrong, try again!';
-    score.textContent--;
+    if (guess > numberCorrect) {
+      document.querySelector('.message').textContent =
+        'Wrong, too high, try again!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'Wrong, too low, try again!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
   }
 };
 
@@ -29,7 +43,7 @@ const resetButton = function () {
   document.querySelector('.score').textContent = 20;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = 0;
-  numberCorrect = Math.floor(Math.random() * 11);
+  numberCorrect = Math.trunc(Math.random() * 10) + 1;
   console.log(numberCorrect);
 };
 
