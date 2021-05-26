5278444a2b88dc1e3579d2259d14d6d6e6a5849a
Hunter Marsh <marshsh@gmail.com>
2021-05-25 23:20:43 -0400

    refactoring practice: ternary operator and function creation for repetitive code

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -41,43 +41,50 @@
 let numberCorrect = Math.trunc(Math.random() * 10) + 1;
 
 let score = 20;
+let highscore = 0;
 
 // Function that is the main game logic.  Checks if a guess was entered.  Compares the .guess.value to the RNG number.  Wrong guesses decrement score.  Correct guesses change message text and compares to highscore.
 // Added a score variable above to keep track of the data with code instead of relying on the DOM to keep track of it.  Also added if a guess was too high or too low, and what happens when score reaches 0.  It checks if the score has reached 0 earlier so that it is no longer possible to win after losing.
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 const numberGuess = function () {
-  const highscore = document.querySelector('.highscore');
   const guess = Number(document.querySelector('.guess').value);
+  // When there is no guess
   if (!guess) {
-    document.querySelector('.message').textContent = 'No number given...';
-  } else if (score <= 1) {
-    document.querySelector('.message').textContent = 'You lost the game';
-    document.querySelector('.score').textContent = 0;
-  } else if (guess === numberCorrect) {
-    document.querySelector('.message').textContent = 'Yes!';
+    displayMessage('No number given...');
+  } // When guess is correct
+  else if (guess === numberCorrect) {
+    displayMessage('Yes!');
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').textContent = numberCorrect;
     document.querySelector('.number').style.width = '45rem';
-    if (highscore.textContent < score) {
-      highscore.textContent = score;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
-  } else {
-    if (guess > numberCorrect) {
-      document.querySelector('.message').textContent =
-        'Wrong, too high, try again!';
+    // When guess is wrong
+  } else if (guess !== numberCorrect) {
+    if (score > 1) {
+      displayMessage(
+        guess > numberCorrect
+          ? 'Too high!  Guess again.'
+          : 'Too low!  Try again.'
+      );
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent =
-        'Wrong, too low, try again!';
-      score--;
-      document.querySelector('.score').textContent = score;
+      displayMessage('You lost the game');
+      document.querySelector('.score').textContent = 0;
     }
   }
 };
 
 // Function to reset the message, reset the score, sets the big number back to '?', resets input field, resets the background color/number width, and re-create the RNG number
 const resetButton = function () {
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.score').textContent = 20;
   score = 20;
   document.querySelector('.number').textContent = '?';
