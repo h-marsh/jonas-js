9cd4f4df726eae0a3a9dd3b66937fe76fc5b1cc2
Hunter Marsh <marshsh@gmail.com>
2021-06-19 19:57:58 -0400

    changed the hold-function to follow the lecture.  did the same method of using a variable to create a dynamic querySelector.  created a state variable to track the game's condition--winning sets it to false which 'deactivates' the buttons since they're wrapped in an if-statement looking at the state variable

--- complete-javascript-course-master/07-Pig-Game/starter/scriptJonas.js
+++ complete-javascript-course-master/07-Pig-Game/starter/scriptJonas.js
@@ -21,25 +21,53 @@ let currentScore = 0;
 diceElement.classList.add('hidden');
 let activePlayer = 0;
 const totalScores = [0, 0];
+let playing = true;
+
+const switchPlayer = function () {
+  currentScore = 0;
+  document.querySelector(`#current--${activePlayer}`).textContent =
+    currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0.classList.toggle('player--active');
+  player1.classList.toggle('player--active');
+};
 
 const roll = function () {
-  // Create random number, display dice image, set source according to number rolled.
-  const numberRolled = Math.trunc(Math.random() * 6) + 1;
-  diceElement.classList.remove('hidden');
-  diceElement.src = `dice-${numberRolled}.png`;
-  // Checking if a 1 is rolled
-  if (numberRolled !== 1) {
-    currentScore += numberRolled;
-    document.querySelector(`#current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Switch to other player
-    currentScore = 0;
-    document.querySelector(`#current--${activePlayer}`).textContent =
-      currentScore;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0.classList.toggle('player--active');
-    player1.classList.toggle('player--active');
+  if (playing) {
+    // Create random number, display dice image, set source according to number rolled.
+    const numberRolled = Math.trunc(Math.random() * 6) + 1;
+    diceElement.classList.remove('hidden');
+    diceElement.src = `dice-${numberRolled}.png`;
+    // Checking if a 1 is rolled
+    if (numberRolled !== 1) {
+      currentScore += numberRolled;
+      document.querySelector(`#current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to other player
+      switchPlayer();
+    }
+  }
+};
+
+const hold = function () {
+  // Add current score to total score (for the current player), check if the score is >=100.  If so, Current player wins, otherwise switch player.
+  if (playing) {
+    totalScores[activePlayer] += currentScore;
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      totalScores[activePlayer];
+    if (totalScores[activePlayer] >= 20) {
+      playing = false;
+      diceElement.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.toggle('active--player');
+    } else {
+      switchPlayer();
+    }
   }
 };
 
@@ -55,31 +83,6 @@ const newGame = function () {
   player0.classList.remove('player--winner');
   player1.classList.remove('player--winner');
   if (!player0.classList.contains('player--active')) {
-    player0.classList.add('player--active');
-    player1.classList.remove('player--active');
-  }
-};
-
-const hold = function () {
-  // Look at the flowchart and follow that logic
-  if (player0.classList.contains('player--active')) {
-    score0 += current0;
-    current0 = 0;
-    current0Element.textContent = current0;
-    score0Element.textContent = score0;
-    if (score0 >= 100) {
-      player0.classList.add('player--winner');
-    }
-    player0.classList.remove('player--active');
-    player1.classList.add('player--active');
-  } else if (player1.classList.contains('player--active')) {
-    score1 += current1;
-    current1 = 0;
-    current1Element.textContent = current1;
-    score1Element.textContent = score1;
-    if (score1 >= 100) {
-      player1.classList.add('player--winner');
-    }
     player0.classList.add('player--active');
     player1.classList.remove('player--active');
   }
