ae5839df9a42399d0cad1219e6a234ab3f170185
Hunter Marsh <marshsh@gmail.com>
2021-06-25 20:28:37 -0400

    practiced some scoping to see the different relationships between scopes in the scope chain

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// This function is defined in the global scope as it is top-level code.  It also creates its own scope.
+function calcAge(birthYear) {
+  const age = 2021 - birthYear;
+  function printAge() {
+    const output = `${firstName}, you are ${age} years old, born in ${birthYear}.`;
+    console.log(output);
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh and you're a millennial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    add(1, 2);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
