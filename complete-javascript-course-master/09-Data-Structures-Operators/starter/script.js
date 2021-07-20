79671ff95014d2c5a20efad08f663c61a145f312
Hunter Marsh <marshsh@gmail.com>
2021-07-20 17:50:31 -0400

    destructuring array intro

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,11 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  // Returns an array that contains the items that were specified by their index positions in their respective menu arrays
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +32,18 @@ const restaurant = {
     },
   },
 };
+
+// Recieve 2 return values from a function, even though the function only returned one item (an array).  This is a good way to immediately creating two variables out of one fcuntion call.
+const [starterItem, mainItem] = restaurant.order(2, 0);
+console.log(starterItem, mainItem);
+
+// Nested array destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;  This one returns a number and an array.
+
+// This will return individual items isntead of having one remain the nested array.
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values for unknown array lengths
+const [p, q, r] = [8, 9];
