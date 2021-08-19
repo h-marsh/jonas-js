a59b7d8aa991ba34fc10c3415602ce0970ad95a7
Hunter Marsh <marshsh@gmail.com>
2021-08-19 12:41:13 -0400

    big ole string methods practice.  dont forget to focus on extracting data before worrying about its formatting

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -640,9 +640,6 @@ const hoursMap = new Map(Object.entries(openingHours));
 ///////////////////////////////////// Working with Strings - pt 1 /////////////////////////////////////
 ///////////////////////////////////////////                 ///////////////////////////////////////////
 
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 const airline = 'TAP Air Portugal';
 // const plane = 'A320';
 
@@ -798,4 +795,52 @@ const maskCard = function (number) {
 
 // Repeat()
 const messageWarn = 'Bad weather... All Departures Delayed... ';
-console.log(messageWarn.repeat(3));
+// console.log(messageWarn.repeat(3));
+
+///////////////////////////////////////////                 ///////////////////////////////////////////
+/////////////////////////////////////// String Methods Practice ///////////////////////////////////////
+///////////////////////////////////////////                 ///////////////////////////////////////////
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+//  * Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//     * Delayed Arrive from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+// const flightsArray = flights.replace(/;/g, ' ').replace(/_/g, ' ').split('+');
+// console.log(flightsArray);
+
+// for (const flights of flightsArray) {
+//   const flight = flights.split('_');
+//   // console.log(flight);
+//   for (const entry of flight) {
+//     const individualFlight = entry.trim().split(' ');
+//     // console.log(individualFlight);
+//     for (const data of individualFlight) {
+//       console.log(data);
+//     }
+//   }
+// }
+
+//  * Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//     * Delayed Arrive from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+// Jonas' solution //
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '*' : ''}${type.replace(
+    /_/g,
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(45);
+  console.log(output);
+}
