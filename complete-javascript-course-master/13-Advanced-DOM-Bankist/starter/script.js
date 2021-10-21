538d57c878f798f17667ce525211c11d0ac5fd8d
Hunter Marsh <marshsh@gmail.com>
2021-10-20 20:52:32 -0400

    bind() is used to pass a psuedo-argument into a handler callback function

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -69,6 +69,27 @@ tabsContainer.addEventListener('click', function (event) {
     .classList.add('operations__content--active');
 });
 
+/* menu link fade animation */
+const nav = document.querySelector('.nav');
+
+const handleHover = function (event) {
+  if (event.target.classList.contains('nav__link')) {
+    const link = event.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(element => {
+      if (element !== link) element.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+/* bind() is used to pass a psuedo-argument into a handler callback function */
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
+
 ///////////////////////////////////////////                 ///////////////////////////////////////////
 //////////////////////////////////////////////// NOTES ////////////////////////////////////////////////
 ///////////////////////////////////////////                 ///////////////////////////////////////////
