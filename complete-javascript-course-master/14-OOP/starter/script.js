faa5969c7e0970bc915d9bc12ffe4cdf4ba10308
Hunter Marsh <marshsh@gmail.com>
2021-11-11 12:12:30 -0500

    inheritance between classes intro

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -158,16 +158,110 @@
 /////////////////////////////////////////// Static Methods  ///////////////////////////////////////////
 ///////////////////////////////////////////                 ///////////////////////////////////////////
 
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
+
+// Person.hey = function () {
+//   console.log('Hello!!');
+// };
+
+// // Person.hey();
+
+// class PersonClass {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
+
+//   calcAge() {
+//     console.log(2021 - this.birthYear);
+//   }
+
+//   greet() {
+//     console.log(`Hello, ${this.fullName}`);
+//   }
+
+//   get age() {
+//     return 2021 - this.birthYear;
+//   }
+
+//   set fullName(name) {
+//     if (name.includes(' ')) this._fullName = name;
+//     else alert(`${name} is not a full name, bruh.`);
+//   }
+
+//   get fullName() {
+//     return this._fullName;
+//   }
+
+//   static hey = function () {
+//     console.log('Hello!!');
+//   };
+// }
+
+// PersonClass.hey();
+
+///////////////////////////////////////////                 ///////////////////////////////////////////
+///////////////////////////////////////////  Object.create  ///////////////////////////////////////////
+///////////////////////////////////////////                 ///////////////////////////////////////////
+
+// const PersonPrototype = {
+//   calcAge() {
+//     console.log(2021 - this.birthYear);
+//   },
+
+//   init(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   },
+// };
+
+// const steven = Object.create(PersonPrototype);
+
+// // these properties were only added this way for the sake of the exercise.  normally the object and its properties would be created programmatically.
+// steven.name = 'steven';
+// steven.birthYear = 1988;
+
+// const sarah = Object.create(PersonPrototype);
+
+// sarah.init('Sarah Smith', 1984);
+
+// console.log(sarah);
+// sarah.calcAge();
+
+///////////////////////////////////////////                 ///////////////////////////////////////////
+//////////////////////// Inheritance Between 'Classes': Constructor Functions ////////////////////////
+///////////////////////////////////////////                 ///////////////////////////////////////////
+
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
 };
 
-Person.hey = function () {
-  console.log('Hello!!');
+Person.prototype.calcAge = function () {
+  console.log(2021 - this.birthYear);
 };
 
-// Person.hey();
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+// mike.calcAge();
+
+///////////////////////////////////////////                 ///////////////////////////////////////////
+////////////////////////////// Inheritance Between 'Classes': ES6 Classes //////////////////////////////
+///////////////////////////////////////////                 ///////////////////////////////////////////
 
 class PersonClass {
   constructor(fullName, birthYear) {
@@ -201,10 +295,20 @@ class PersonClass {
   };
 }
 
-// PersonClass.hey();
+class StudentClass extends PersonClass {
+  constructor(fullName, birthYear, course) {
+    /* the super call always happens first  */
+    super(fullName, birthYear);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+}
 
 ///////////////////////////////////////////                 ///////////////////////////////////////////
-///////////////////////////////////////////  Object.create  ///////////////////////////////////////////
+///////////////////////////// Inheritance Between 'Classes': Object.create /////////////////////////////
 ///////////////////////////////////////////                 ///////////////////////////////////////////
 
 const PersonPrototype = {
@@ -219,14 +323,3 @@ const PersonPrototype = {
 };
 
 const steven = Object.create(PersonPrototype);
-
-// these properties were only added this way for the sake of the exercise.  normally the object and its properties would be created programmatically.
-steven.name = 'steven';
-steven.birthYear = 1988;
-
-const sarah = Object.create(PersonPrototype);
-
-sarah.init('Sarah Smith', 1984);
-
-console.log(sarah);
-sarah.calcAge();
