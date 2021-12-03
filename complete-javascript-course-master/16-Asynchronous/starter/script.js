11a981a692c5af0eae0053bbcf4df70caa813626
Hunter Marsh <marshsh@gmail.com>
2021-12-03 18:52:18 -0500

    callback hell...bad for code because its messy, makes code hard to understand and reason, harder to maintain so more bugs, and harder to add features/updates.  watch out for those pyramids!

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -9,15 +9,46 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////////////  NOTES  ///////////////////////////////////////////////
 ///////////////////////////////////////////                 ///////////////////////////////////////////
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  request.send();
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
 
-    const html = `
-    <article class="country">
+//     const html = `
+//     <article class="country">
+//         <img class="country__img" src="${data.flag}" />
+//         <div class="country__data">
+//             <h3 class="country__name">${data.name}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//               Number(data.population) / 1000000
+//             ).toFixed(1)} million people</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//             <p class="country__row"><span>💰</span>${
+//               data.currencies[0].code
+//             }</p>
+//         </div>
+//     </article>
+//     `;
+
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// getCountryData('spain');
+// getCountryData('canada');
+// getCountryData('japan');
+
+///////////////////////////////////////////                 ///////////////////////////////////////////
+///////////////////////////////////////////  Callback Hell  ///////////////////////////////////////////
+///////////////////////////////////////////                 ///////////////////////////////////////////
+
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
         <img class="country__img" src="${data.flag}" />
         <div class="country__data">
             <h3 class="country__name">${data.name}</h3>
@@ -33,11 +64,36 @@ const getCountryData = function (country) {
     </article>
     `;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryDataAndNeighbor = function (country) {
+  /* ajax call country 1 */
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+
+    /* render country 1 */
+    renderCountry(data);
+
+    /* neighboring country (2) */
+    if (!data.borders) return; /* in case the country is an island */
+    const [neighbor] = data.borders;
+
+    /* ajax call country 2 */
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbor}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('spain');
-getCountryData('canada');
-getCountryData('japan');
+getCountryDataAndNeighbor('saudi arabia');
