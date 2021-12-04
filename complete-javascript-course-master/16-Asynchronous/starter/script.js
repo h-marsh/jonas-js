8150676867db66781d415afdb5f54130eaf4a752
Hunter Marsh <marshsh@gmail.com>
2021-12-03 21:10:15 -0500

    intro to promises and the fetch api

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -9,34 +9,34 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////////////  NOTES  ///////////////////////////////////////////////
 ///////////////////////////////////////////                 ///////////////////////////////////////////
 
-// const getCountryData = function (country) {
-//   const request = new XMLHttpRequest();
-//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
-//   request.send();
-//   request.addEventListener('load', function () {
-//     const [data] = JSON.parse(this.responseText);
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
 
-//     const html = `
-//     <article class="country">
-//         <img class="country__img" src="${data.flag}" />
-//         <div class="country__data">
-//             <h3 class="country__name">${data.name}</h3>
-//             <h4 class="country__region">${data.region}</h4>
-//             <p class="country__row"><span>👫</span>${(
-//               Number(data.population) / 1000000
-//             ).toFixed(1)} million people</p>
-//             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//             <p class="country__row"><span>💰</span>${
-//               data.currencies[0].code
-//             }</p>
-//         </div>
-//     </article>
-//     `;
+    const html = `
+    <article class="country">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              Number(data.population) / 1000000
+            ).toFixed(1)} million people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].code
+            }</p>
+        </div>
+    </article>
+    `;
 
-//     countriesContainer.insertAdjacentHTML('beforeend', html);
-//     countriesContainer.style.opacity = 1;
-//   });
-// };
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
 
 // getCountryData('spain');
 // getCountryData('canada');
@@ -96,4 +96,16 @@ const getCountryDataAndNeighbor = function (country) {
   });
 };
 
-getCountryDataAndNeighbor('saudi arabia');
+// getCountryDataAndNeighbor('saudi arabia');
+
+///////////////////////////////////////////                 ///////////////////////////////////////////
+////////////////////////////////////  Promises and the Fetch API  ////////////////////////////////////
+///////////////////////////////////////////                 ///////////////////////////////////////////
+
+/* old way of AJAX calls */
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v2/name/${country}`);
+// request.send();
+
+/* AJAX calls with Fetch API */
+const request = fetch('https://restcountries.com/v2/name/portugal');
