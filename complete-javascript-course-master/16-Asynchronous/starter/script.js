55c0414ea7ad358b6b8970c6d6be9f081be58393
Hunter Marsh <marshsh@gmail.com>
2021-12-20 22:03:22 -0500

    callback queue vs microtask (promise callback) queue priority with event loop

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+// const btn = document.querySelector('.btn-country');
+// const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
@@ -254,50 +254,65 @@ const displayError = function (message) {
 ///////////////////////////////////////  Coding Challenge #1   ///////////////////////////////////////
 ///////////////////////////////////////////                 ///////////////////////////////////////////
 
-const renderCountry = function (data, className = '') {
-  const html = `
-    <article class="country ${className}">
-        <img class="country__img" src="${data.flag}" />
-        <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              Number(data.population) / 1000000
-            ).toFixed(1)} million people</p>
-            <p class="country__row"><span>😃</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${
-              data.currencies[0].code
-            }</p>
-        </div>
-    </article>
-    `;
+// const renderCountry = function (data, className = '') {
+//   const html = `
+//     <article class="country ${className}">
+//         <img class="country__img" src="${data.flag}" />
+//         <div class="country__data">
+//             <h3 class="country__name">${data.name}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//               Number(data.population) / 1000000
+//             ).toFixed(1)} million people</p>
+//             <p class="country__row"><span>😃</span>${data.languages[0].name}</p>
+//             <p class="country__row"><span>💰</span>${
+//               data.currencies[0].code
+//             }</p>
+//         </div>
+//     </article>
+//     `;
 
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-};
+//   countriesContainer.insertAdjacentHTML('beforeend', html);
+// };
 
-/* this will get the relevant geo-info based on provided coordinates */
-const whereAmI = function (lat, lng) {
-  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-    .then(response => {
-      if (!response.ok)
-        throw new Error(`Too many requests: (${response.status})`);
-      return response.json();
-    })
-    .then(data => fetch(`https://restcountries.com/v2/name/${data.country}`))
-    .then(response => {
-      if (!response.ok)
-        throw new Error(`Country not found: (${response.status})`);
-      return response.json();
-    })
-    .then(data => renderCountry(data[0]))
-    .catch(error => console.error(`${error}😢`))
-    .finally(() => (countriesContainer.style.opacity = 1));
-};
+// /* this will get the relevant geo-info based on provided coordinates */
+// const whereAmI = function (lat, lng) {
+//   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//     .then(response => {
+//       if (!response.ok)
+//         throw new Error(`Too many requests: (${response.status})`);
+//       return response.json();
+//     })
+//     .then(data => fetch(`https://restcountries.com/v2/name/${data.country}`))
+//     .then(response => {
+//       if (!response.ok)
+//         throw new Error(`Country not found: (${response.status})`);
+//       return response.json();
+//     })
+//     .then(data => renderCountry(data[0]))
+//     .catch(error => console.error(`${error}😢`))
+//     .finally(() => (countriesContainer.style.opacity = 1));
+// };
 
-btn.addEventListener('click', function () {
-  whereAmI(-33.933, 18.474);
-});
+// btn.addEventListener('click', function () {
+//   whereAmI(-33.933, 18.474);
+// });
 
 // whereAmI(' 52.508', '13.381 ');
 // whereAmI(' 19.037', '72.873');
 // whereAmI('-33.933', '18.474');
+
+///////////////////////////////////////////                 ///////////////////////////////////////////
+////////////////////////////////////  The Event Loop in Practice   ////////////////////////////////////
+///////////////////////////////////////////                 ///////////////////////////////////////////
+
+console.log('Test start');
+setTimeout(() => console.log('0 second timer'), 0);
+Promise.resolve('Resolved promise 1').then(response => console.log(response));
+
+Promise.resolve('Resolved promise 2').then(response => {
+  for (let i = 0; i < 10000000000; i++) {}
+  console.log(response);
+});
+
+console.log('Test end');
