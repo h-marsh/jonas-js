e4b95214a51947f20e30de2c9e4ba61d77390331
Hunter Marsh <marshsh@gmail.com>
2022-01-09 21:58:09 -0500

    importing and exporting with es6 modules.  named exports = ezpz and good for multiple exports at once.  default imports = when only one thing is being exported from the module, given a name when it is imported.

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,4 +1,22 @@
-'use strict';
+// 'use strict';
 ///////////////////////////////////////////                 ///////////////////////////////////////////
-///////////////////////////////////////////////  NOTES  ///////////////////////////////////////////////
+///////////////////////////////  Exporting and Importing in ES6 Modules  ///////////////////////////////
 ///////////////////////////////////////////                 ///////////////////////////////////////////
+
+/* importing a module without importing a value */
+
+// this is the importing module.
+
+// import { addToCart, totalPrice as price, totalQuantity } from './shoppingCart.js';
+// console.log(price, totalQuantity);
+// addToCart('eggs', 12);
+
+console.log('++Importing Module++');
+
+// import * as ShoppingCart from './shoppingCart.js';
+// console.log(ShoppingCart.totalQuantity);
+// console.log(ShoppingCart.totalPrice);
+// ShoppingCart.addToCart('eggs', 12);
+
+import add from './shoppingCart.js';
+add('bread', 2);
---
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,20 @@
+// this is the exporting module.
+console.log('++Exporting Module++');
+
+const shippingCost = 10;
+const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} was added to the cart!`);
+};
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity };
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} was added to the cart!`);
+}
