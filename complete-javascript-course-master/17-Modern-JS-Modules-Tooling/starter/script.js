902911910cea3d15f3f34166668c3efccdd067ac
Hunter Marsh <marshsh@gmail.com>
2022-01-10 19:31:28 -0500

    top level await is using await outside of an async function.  this can cause blocking since execution has to await...(duh).  though it can be useful for handling values returned from async functions since async functions always return promises that need to be handled

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,9 +1,11 @@
 // 'use strict';
+// import fetch from 'node-fetch';
 ///////////////////////////////////////////                 ///////////////////////////////////////////
 ///////////////////////////////  Exporting and Importing in ES6 Modules  ///////////////////////////////
 ///////////////////////////////////////////                 ///////////////////////////////////////////
 
 /* importing a module without importing a value */
+console.log('++Importing Module++');
 
 // this is the importing module.
 
@@ -11,16 +13,33 @@
 // console.log(price, totalQuantity);
 // addToCart('eggs', 12);
 
-console.log('++Importing Module++');
-
 // import * as ShoppingCart from './shoppingCart.js';
 // console.log(ShoppingCart.totalQuantity);
 // console.log(ShoppingCart.totalPrice);
 // ShoppingCart.addToCart('eggs', 12);
 
-import add from './shoppingCart.js';
-add('bread', 2);
+// import add from './shoppingCart.js';
+// add('bread', 2);
 
 ///////////////////////////////////////////                 ///////////////////////////////////////////
 /////////////////////////////////////  Top-Level await (ES2022)  /////////////////////////////////////
 ///////////////////////////////////////////                 ///////////////////////////////////////////
+
+// const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await response.json();
+// console.log(data);
+
+const getLastPost = async function () {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+
+// Not clean
+// lastPost.then(last => console.log(last));
+
+const lastPostAwait = await getLastPost();
+console.log(lastPostAwait);
