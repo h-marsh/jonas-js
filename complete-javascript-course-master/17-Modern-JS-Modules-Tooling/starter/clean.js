48964cf5a5112e5978019d4e138ac4e52bc0f12e
Hunter Marsh <marshsh@gmail.com>
2022-01-18 22:27:58 -0500

    looked at more fuinctional/declarative principles.  Remember, always pass in any data a function needs.  Do not access any array, object, etc that is outside the function from inside the function.  Pass the data in, create a copy by storing that data into a new variable/return statement, and return something new that is independent from the data that was passed in

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/clean.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,4 +1,6 @@
-const budget = [
+'use strict';
+
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,46 +9,70 @@ const budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-const spendingLimits = {
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
+});
+
+const getLimit = (limits, user) => (limits[user] ? limits[user] : 0);
+// const getLimit = user => spendingLimits?.[user] ?? 0; // optional chaining checks if the property exists, nullish coalescing operator instructs to do something else if its undefined.
+
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'jonas'
+) {
+  const cleanUser = user.toLowerCase();
+
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value: -value, description, user: cleanUser }]
+    : state;
 };
 
-const getLimit = user => (spendingLimits[user] ? spendingLimits[user] : 0);
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  100,
+  'Going to movies 🍿',
+  'Matilda'
+);
+const newBudget3 = addExpense(budget, spendingLimits, 200, 'Stuff', 'Jay');
 
-const addExpense = function (value, description, user = 'jonas') {
-  user = user.toLowerCase();
-
-  // const limit = spendingLimits?.[user] ?? 0; // optional chaining checks if the property exists, nullish coalescing operator instructs to do something else if its undefined.
-
-  if (value <= getLimit(user)) {
-    budget.push({ value: -value, description, user });
-  }
-};
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
-
-const checkExpenses = function () {
-  for (const entry of budget) {
-    if (entry.value < -getLimit(entry.user)) {
-      entry.flag = 'limit';
-    }
-  }
-};
-checkExpenses();
-
-const logBigExpenses = function (overLimit) {
-  let output = '';
-  for (const entry of budget)
-    output +=
-      entry.value <= -overLimit ? `${entry.description.slice(-2)} / ` : '';
-
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
+const checkExpenses = function (state, limits) {
+  return state.map(entry => {
+    return entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: 'limit' }
+      : entry;
+  });
+  // for (const entry of state) {
+  //   if (entry.value < -getLimit(limits, entry.user)) {
+  //     entry.flag = 'limit';
+  //   }
+  // }
 };
 
-console.log(budget);
-logBigExpenses(1000);
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+
+const logBigExpenses = function (state, overLimit) {
+  const bigExpenses = state
+    .filter(entry => entry.value <= -overLimit)
+    .map(entry => entry.description.slice(-2))
+    .join(' / ');
+
+  console.log(bigExpenses);
+
+  // let output = '';
+  // for (const entry of budget)
+  //   output +=
+  //     entry.value <= -overLimit ? `${entry.description.slice(-2)} / ` : '';
+
+  // output = output.slice(0, -2); // Remove last '/ '
+  // console.log(output);
+};
+
+logBigExpenses(finalBudget, 1000);
