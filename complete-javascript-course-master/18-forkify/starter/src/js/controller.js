8c79d4366e6794a7af88851a437c9b672ebd4b9e
Hunter Marsh <marshsh@gmail.com>
2022-01-31 20:36:00 -0500

    refactored by creating a parent view class to avoid duplicate methods between all of the different views (the individual 'child' views will extend this parent class to get access to all of its methods), while keeping some methods, like generating HTML, specific to each unqiue view.  created a resultsView file responsible for rendering the results in a list on the UI.  added an error message for the resultsview in case there are no results for the query

--- complete-javascript-course-master/18-forkify/starter/src/js/controller.js
+++ complete-javascript-course-master/18-forkify/starter/src/js/controller.js
@@ -3,9 +3,16 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
+
+// hot module swapping coming from parcel
+if (module.hot) {
+  module.hot.accept;
+}
 
 const controlRecipes = async function () {
   try {
@@ -27,6 +34,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -35,7 +44,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // render results
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (error) {
     console.log(error);
   }
